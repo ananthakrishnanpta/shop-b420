6b4bfed7231c6d361a27ed6a80ac6615a3cedaee
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2025-04-11 09:09:34 +0530

    edits

--- zshop/cart/models.py
+++ zshop/cart/models.py
@@ -10,15 +10,12 @@ from django.contrib.auth.models import User
 class CartItem(models.Model):
 
     product = models.ForeignKey(Product, on_delete = models.CASCADE)
-
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-
-    quantity = models.IntegerField(default=0)
-    # INTEGER
-    
-    date_added = models.DateTimeField(auto_now_add=True)
-    # Current timestamp
-
+    quantity = models.IntegerField(default=0) # INTEGER
+    date_added = models.DateTimeField(auto_now_add=True) # Current timestamp
 
     def __str__(self):
         return f"{self.product.name} added by {self.user.username} at {self.date_added}"
+    
+    def get_total(self):
+        return self.product.price * self.quantity
--- zshop/cart/views.py
+++ zshop/cart/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from .models import CartItem
+from mainapp.models import Product
 
 
 # Create your views here
 
 def viewCart(request):
-    cartItems = CartItem.objects.filter(user = request.user)
+
+    # select * from CartItem where user = request.user; 
+    # => this gives reference to collection of cart items (filter)
+    cartItems = CartItem.objects.filter(user = request.user)  
 
     template = 'cart.html'
 
@@ -15,3 +19,16 @@ def viewCart(request):
     }
 
     return render(request, template, context)
+
+def addToCart(request, product_id):
+
+    # select * from Product where id = product_id; 
+    # => Gives reference to individual object (get)
+    this_product = Product.objects.get(id = product_id)
+    
+    cart_item, created_at = CartItem.objects.get_or_create(product = this_product, user = request.user)
+    cart_item.quantity += 1
+
+    cart_item.save() # insert or update the particular record
+
+    return redirect('view_cart')
