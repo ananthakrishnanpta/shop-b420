96480d9385c84cc7ecfcf8d4208788ce4668a1d2
ananthakrishnanpta <ananthakrishnanpta@gmailcom>
2025-04-16 08:05:04 +0530

    edits

--- zshop/cart/views.py
+++ zshop/cart/views.py
@@ -3,23 +3,26 @@ from django.template import loader
 from .models import CartItem
 from mainapp.models import Product
 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here
-
+@login_required
 def viewCart(request):
 
     # select * from CartItem where user = request.user; 
     # => this gives reference to collection of cart items (filter)
     cartItems = CartItem.objects.filter(user = request.user)  
-
+    total_price = sum([float(item.product.price) * item.quantity for item in cartItems])
     template = 'cart.html'
 
     context = {
-        'items' : cartItems
+        'items' : cartItems,
+        'total' : total_price
     }
 
     return render(request, template, context)
 
+@login_required
 def addToCart(request, product_id):
 
     # select * from Product where id = product_id; 
@@ -30,5 +33,12 @@ def addToCart(request, product_id):
     cart_item.quantity += 1
 
     cart_item.save() # insert or update the particular record
+
+    return redirect('view_cart')
+
+@login_required
+def remFromCart(request,cart_item_id):
+    this_cart_item = CartItem.objects.get(id = cart_item_id)
+    this_cart_item.delete() # this will delete the cart_item_object and its associated record in the CartItem table in db
 
     return redirect('view_cart')
